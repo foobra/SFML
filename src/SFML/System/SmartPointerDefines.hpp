84a90bc15954d0414e8dc01f495085b37499a8e5
pinguo_gaoshan <vipgs99@gmail.com>
2017-08-28 00:57:46 +0800

    Add optional macros

--- src/SFML/System/SmartPointerDefines.hpp
+++ src/SFML/System/SmartPointerDefines.hpp
@@ -27,4 +27,15 @@ static_assert(std::is_same<std::shared_ptr<int>, SP<int>>::value);
 static_assert(std::is_same<std::unique_ptr<int>, UP<int>>::value);
 static_assert(std::is_same<std::weak_ptr<int>, WP<int>>::value);
 
+
+
+#if defined(SFML_SYSTEM_WINDOWS)
+#include <optional>
+template<typename T> using OP = std::optional<T>;
+#else
+#include <experimental/optional>
+template<typename T> using OP = std::experimental::optional<T>;
+#endif
+
+
 #endif //SFML_SMARTPOINTERDEFINES_HPP
